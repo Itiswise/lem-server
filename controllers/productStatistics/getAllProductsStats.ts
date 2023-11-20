9e05d46d2aa2a784fe3d44c797d521d5da3579d9
jakub-gradowski-wisepeople <j.gradowski@itiswise.com>
2023-11-20 14:10:41 +0100

    partnumbers pagination

--- controllers/productStatistics/getAllProductsStats.ts
+++ controllers/productStatistics/getAllProductsStats.ts
@@ -6,11 +6,27 @@ export const getAllProductsStats = function (
   res: Response,
   next: NextFunction
 ): void {
-  ProductStatistics.find({}, function (err, productsStats) {
+  var limit = typeof req.query.limit === "string" ? parseInt(req.query.limit) : 0;
+  var skip = typeof req.query.skip === "string" ? parseInt(req.query.skip) : 0;
+  var search = typeof req.query.search === "string" ? req.query.search : "";
+  var params = { partNumber: { $regex: search, $options: "i", $ne : null }};
+
+  ProductStatistics.find(params, function (err, productsStats) {
     if (err) {
       return next(err);
     }
 
-    res.send(productsStats);
-  });
+    try {
+      ProductStatistics.countDocuments(params, (err, count) => {
+        if (err) {
+          return next(err);
+        }
+
+        res.send({partnumbers: productsStats, allPartnumbersCount: count});
+      });
+    }
+    catch (err) {
+      return next(err);
+    }
+  }).limit(limit).skip(skip).sort({ partNumber: 1 });
 };
