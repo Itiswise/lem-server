b0a01270ff8bc4591e2d906a828b6b2dc7efa20b
jakub-gradowski-wisepeople <j.gradowski@itiswise.com>
2023-11-21 09:02:08 +0100

    orders pagination

--- controllers/order/getOrdersWithStats.ts
+++ controllers/order/getOrdersWithStats.ts
@@ -2,46 +2,104 @@ import { Request, Response, NextFunction } from "express";
 import { Order } from "../../models/order";
 import { Line } from "../../models/line";
 import { getOrderDetails } from "../../services/getOrderDetails";
+import {Product} from "../../models/product";
 
 export const getOrdersWithStats = function (
   req: Request,
   res: Response,
   next: NextFunction
 ): void {
+  var limit = typeof req.query.limit === "string" ? parseInt(req.query.limit) : 0;
+  var skip = typeof req.query.skip === "string" ? parseInt(req.query.skip) : 0;
+  var column = typeof req.query.column === "string" ? req.query.column : 'orderAddedAt';
+  var {
+    orderAddedAt ,
+    orderNumber,
+    orderStatus,
+    partNumber,
+    quantity,
+    order
+  } = req.query;
+  var params = {};
+
+  if (orderAddedAt && typeof orderAddedAt === "string") {
+    var date = orderAddedAt.replace(/\./g, "-");
+    if (!isNaN(Date.parse(orderAddedAt))) {
+      var from = new Date(date);
+      var to = new Date(date);
+
+      switch (date.split("-").length) {
+        case 1:
+          to.setFullYear(from.getFullYear() + 1);
+          break;
+        case 2:
+          to.setMonth(from.getMonth() + 1);
+          break;
+        default:
+          to.setDate(from.getDate() + 1);
+          break;
+      }
+
+      params = { ...params, orderAddedAt: { $gte: from, $lt: to } };
+    }
+  }
+
+  if (orderNumber && typeof orderNumber === "string") {
+      params = { ...params, orderNumber: { $regex: orderNumber, $options: "i" } };
+  }
+
+  if (orderStatus && typeof orderStatus === "string") {
+      params = { ...params, orderStatus: { $regex: orderStatus, $options: "i" } };
+  }
+
+  if (partNumber && typeof partNumber === "string") {
+      params = { ...params, partNumber: { $regex: partNumber, $options: "i" } };
+  }
+
+  if (quantity && typeof quantity === "string") {
+      params = { ...params, quantity: { $gte: parseInt(quantity), $lt: parseInt(quantity) + 1 } };
+  }
+
   Line.find({}, function (err, lines) {
     if (err) {
       return next(err);
     }
     Order.find(
-      {},
+      params,
       "orderNumber orderStatus _id quantity partNumber qrCode orderAddedAt customer",
       function (err, orders) {
         if (err) {
           return next(err);
         }
 
-        const ordersWithStats = orders.map((order) => {
-          const {
-            orderNumber,
-            _id,
-            partNumber,
-            orderStatus,
-            quantity,
-            orderAddedAt,
-          } = getOrderDetails(order, lines);
+        Order.countDocuments(params, (err, count) => {
+          if (err) {
+            return next(err);
+          }
 
-          return {
-            orderNumber,
-            _id,
-            partNumber,
-            orderStatus,
-            quantity,
-            orderAddedAt,
-          };
+          const ordersWithStats = orders.map((order) => {
+            const {
+              orderNumber,
+              _id,
+              partNumber,
+              orderStatus,
+              quantity,
+              orderAddedAt,
+            } = getOrderDetails(order, lines);
+
+            return {
+              orderNumber,
+              _id,
+              partNumber,
+              orderStatus,
+              quantity,
+              orderAddedAt,
+            };
+          });
+
+          res.json({ orders: ordersWithStats, allOrdersCount: count });
         });
-
-        res.json(ordersWithStats);
       }
-    ).sort({ orderAddedAt: -1 });
+    ).sort({ [column]: order === 'asc' ? 1 : -1 }).limit(limit).skip(skip);
   });
 };
