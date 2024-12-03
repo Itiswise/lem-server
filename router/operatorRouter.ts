4281d4e4546e3932dd3cb9665a55191260f49260
SebastianBurzych <s.burzych@itiswise.com>
2024-12-03 15:56:56 +0100

    fix typo - adding missing slash to one of routes in operator router

--- router/operatorRouter.ts
+++ router/operatorRouter.ts
@@ -3,7 +3,7 @@ import { OperatorController } from "../controllers";
 import { requireAuth } from "../services/requireAuth";
 
 export const operatorRouter = function (app: Express) {
-    app.get("api/operator", requireAuth, OperatorController.getOperators);
+    app.get("/api/operator", requireAuth, OperatorController.getOperators);
     app.get("/api/operator/:_id", requireAuth, OperatorController.getOperator);
     app.post("/api/operator", requireAuth, OperatorController.addOperator);
     app.put("/api/operator/:_id", requireAuth, OperatorController.changeOperator);
