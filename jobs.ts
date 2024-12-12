09ab49e5650484867bd8c35109449438f831e330
Sebastian Burzych <105776485+SebastianBurzych@users.noreply.github.com>
2024-12-12 10:03:57 +0100

    Feature/cron (#7)
    * reverse list of operators
    
    * add validation when deleting an operator
    
    * add job scheduling

---
+++ jobs.ts
@@ -0,0 +1,35 @@
+import {Agenda} from "@hokify/agenda";
+import { keys } from "./config/keys";
+import {pauseOrdersJob} from "./jobs/pauseOrdersJob";
+import {Logger} from "winston";
+
+export const initAgenda = async function(logger: Logger): Promise<void> {
+    const agenda = new Agenda(
+        {
+            db: {
+                address: keys.dbAtlas,
+                collection: "agendaJobs",
+            }
+        }
+    );
+
+    const scheduledHour = 13;
+    const cronExpression = `0 ${scheduledHour} * * *`;
+
+    agenda.define('pause_orders', async function() {
+      await pauseOrdersJob(logger);
+    });
+
+    await agenda.start();
+
+    await agenda.every(cronExpression, 'pause_orders');
+
+    async function graceful() {
+        await agenda.stop();
+        await agenda.cancel({name: 'pause_orders'});
+        process.exit(0);
+    }
+
+    process.on('SIGTERM', graceful);
+    process.on('SIGINT', graceful);
+}
