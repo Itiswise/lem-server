dde4ad97cbbeef7684978622549c4ae30ca5d34d
SebastianBurzych <s.burzych@itiswise.com>
2025-01-23 11:17:03 +0100

    reverse month rendering

--- services/renderTime.ts
+++ services/renderTime.ts
@@ -6,7 +6,7 @@ export const renderTime = (time: number | Date) => {
   }
   const localTime = new Date(time);
   const year = concatenateZeroIfLessThanTen(localTime.getFullYear());
-  const month = concatenateZeroIfLessThanTen(localTime.getMonth());
+  const month = concatenateZeroIfLessThanTen(localTime.getMonth() + 1);
   const day = concatenateZeroIfLessThanTen(localTime.getDate());
   const hours = concatenateZeroIfLessThanTen(localTime.getHours());
   const minutes = concatenateZeroIfLessThanTen(localTime.getMinutes());
